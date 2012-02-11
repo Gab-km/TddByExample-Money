90962d320986b0b390c01291ededb09576a0e1f0
Gab_km <the25thcromosome@gmail.com>
2012-02-11 15:51:22 +0900

    [from now] Sat Feb 11 15:51:23     2012
    diff --git a/bin/jp/gab_km/Franc.class b/bin/jp/gab_km/Franc.class
    new file mode 100644
    index 0000000..d474e2b
    Binary files /dev/null and b/bin/jp/gab_km/Franc.class differ
    diff --git a/bin/jp/gab_km/FrancTest.class b/bin/jp/gab_km/FrancTest.class
    new file mode 100644
    index 0000000..3a3dafc
    Binary files /dev/null and b/bin/jp/gab_km/FrancTest.class differ
    diff --git a/src/jp/gab_km/Franc.java b/src/jp/gab_km/Franc.java
    new file mode 100644
    index 0000000..430aa0c
    --- /dev/null
    +++ b/src/jp/gab_km/Franc.java
    @@ -0,0 +1,14 @@
    +package jp.gab_km;
    +
    +public class Franc {
    +
    +	public Franc(int i) {
    +		// TODO Auto-generated constructor stub
    +	}
    +
    +	public Object times(int i) {
    +		// TODO Auto-generated method stub
    +		return null;
    +	}
    +
    +}
    diff --git a/src/jp/gab_km/FrancTest.java b/src/jp/gab_km/FrancTest.java
    new file mode 100644
    index 0000000..cf8d67f
    --- /dev/null
    +++ b/src/jp/gab_km/FrancTest.java
    @@ -0,0 +1,16 @@
    +package jp.gab_km;
    +
    +import static org.junit.Assert.*;
    +
    +import org.junit.Test;
    +
    +public class FrancTest {
    +
    +	@Test
    +	public void testFrancMultiplication(){
    +		Franc five = new Franc(5);
    +		assertEquals(new Franc(10), five.times(2));
    +		assertEquals(new Franc(15), five.times(3));
    +	}
    +
    +}

---
+++ src/jp/gab_km/Franc.java
@@ -0,0 +1,14 @@
+package jp.gab_km;
+
+public class Franc {
+
+	public Franc(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object times(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
