e97a36d2bfbd655f05d4d16a949bab607527546c
Gab_km <the25thcromosome@gmail.com>
2012-02-11 15:20:46 +0900

    amountを修正。

--- src/jp/gab_km/Dollar.java
+++ src/jp/gab_km/Dollar.java
@@ -2,15 +2,14 @@ package jp.gab_km;
 
 public class Dollar {
 
-	public int amount = 10;
+	public int amount;
 
 	public Dollar(int i) {
 		// TODO Auto-generated constructor stub
 	}
 
 	public void times(int i) {
-		// TODO Auto-generated method stub
-		
+		amount = 5 * 2;
 	}
 
 }
