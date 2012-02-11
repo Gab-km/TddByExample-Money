c8d1fcb865af4b7dd980dde66c5560fb0e00d789
Gab_km <the25thcromosome@gmail.com>
2012-02-11 15:18:11 +0900

    最初のテストをグリーンにした。

--- src/jp/gab_km/Dollar.java
+++ src/jp/gab_km/Dollar.java
@@ -2,4 +2,15 @@ package jp.gab_km;
 
 public class Dollar {
 
+	public int amount = 10;
+
+	public Dollar(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void times(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
