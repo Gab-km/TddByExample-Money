33b101b48d77520ba222368737a253441e57fea9
Gab_km <the25thcromosome@gmail.com>
2012-02-15 01:54:11 +0900

    [from now] Wed Feb 15 01:54:12     2012
    diff --git a/bin/jp/gab_km/Franc.class b/bin/jp/gab_km/Franc.class
    deleted file mode 100644
    index b854510..0000000
    Binary files a/bin/jp/gab_km/Franc.class and /dev/null differ
    diff --git a/bin/jp/gab_km/MoneyTest.class b/bin/jp/gab_km/MoneyTest.class
    index 3649e0a..1fd0aa9 100644
    Binary files a/bin/jp/gab_km/MoneyTest.class and b/bin/jp/gab_km/MoneyTest.class differ
    diff --git a/src/jp/gab_km/Franc.java b/src/jp/gab_km/Franc.java
    deleted file mode 100644
    index eff655c..0000000
    --- a/src/jp/gab_km/Franc.java
    +++ /dev/null
    @@ -1,9 +0,0 @@
    -package jp.gab_km;
    -
    -public class Franc extends Money {
    -
    -	public Franc(int amount, String currency) {
    -		super(amount, currency);
    -	}
    -
    -}
    diff --git a/src/jp/gab_km/MoneyTest.java b/src/jp/gab_km/MoneyTest.java
    index b18a963..46f0cf8 100644
    --- a/src/jp/gab_km/MoneyTest.java
    +++ b/src/jp/gab_km/MoneyTest.java
    @@ -12,6 +12,13 @@ public class MoneyTest {
     		assertEquals(Money.dollar(10), five.times(2));
     		assertEquals(Money.dollar(15), five.times(3));
     	}
    +
    +	@Test
    +	public void testFrancMultiplication(){
    +		Money five = Money.franc(5);
    +		assertEquals(Money.franc(10), five.times(2));
    +		assertEquals(Money.franc(15), five.times(3));
    +	}
    
     	@Test
     	public void testEquality(){

--- src/jp/gab_km/Franc.java
+++
@@ -1,9 +0,0 @@
-package jp.gab_km;
-
-public class Franc extends Money {
-	
-	public Franc(int amount, String currency) {
-		super(amount, currency);
-	}
-
-}
--- src/jp/gab_km/MoneyTest.java
+++ src/jp/gab_km/MoneyTest.java
@@ -12,6 +12,13 @@ public class MoneyTest {
 		assertEquals(Money.dollar(10), five.times(2));
 		assertEquals(Money.dollar(15), five.times(3));
 	}
+
+	@Test
+	public void testFrancMultiplication(){
+		Money five = Money.franc(5);
+		assertEquals(Money.franc(10), five.times(2));
+		assertEquals(Money.franc(15), five.times(3));
+	}
 	
 	@Test
 	public void testEquality(){
