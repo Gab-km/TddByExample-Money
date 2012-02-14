1141317a3042bddadab033a51da17c6cfae33aaf
Gab_km <the25thcromosome@gmail.com>
2012-02-15 01:52:11 +0900

    [from now] Wed Feb 15 01:52:11     2012
    diff --git a/bin/jp/gab_km/FrancTest.class b/bin/jp/gab_km/FrancTest.class
    index c632a76..e8fdce0 100644
    Binary files a/bin/jp/gab_km/FrancTest.class and b/bin/jp/gab_km/FrancTest.class differ
    diff --git a/src/jp/gab_km/FrancTest.java b/src/jp/gab_km/FrancTest.java
    index 9cb833c..05fc567 100644
    --- a/src/jp/gab_km/FrancTest.java
    +++ b/src/jp/gab_km/FrancTest.java
    @@ -19,10 +19,5 @@ public class FrancTest {
     		assertFalse(Money.franc(5).equals(Money.franc(6)));
     		assertFalse(Money.dollar(5).equals(Money.franc(5)));
     	}
    -
    -	@Test
    -	public void testDifferentClassEquality(){
    -		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
    -	}
    
     }

--- src/jp/gab_km/FrancTest.java
+++ src/jp/gab_km/FrancTest.java
@@ -19,10 +19,5 @@ public class FrancTest {
 		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.dollar(5).equals(Money.franc(5)));
 	}
-	
-	@Test
-	public void testDifferentClassEquality(){
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
 
 }
