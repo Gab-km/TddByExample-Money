0009e4ad069d8effab05d3f95865855fc8c326ee
Gab_km <the25thcromosome@gmail.com>
2012-02-15 01:50:44 +0900

    [from now] Wed Feb 15 01:50:44     2012
    diff --git a/bin/jp/gab_km/Dollar.class b/bin/jp/gab_km/Dollar.class
    deleted file mode 100644
    index 778eafc..0000000
    Binary files a/bin/jp/gab_km/Dollar.class and /dev/null differ
    diff --git a/bin/jp/gab_km/DollarTest.class b/bin/jp/gab_km/DollarTest.class
    deleted file mode 100644
    index 678f110..0000000
    Binary files a/bin/jp/gab_km/DollarTest.class and /dev/null differ
    diff --git a/bin/jp/gab_km/MoneyTest.class b/bin/jp/gab_km/MoneyTest.class
    new file mode 100644
    index 0000000..3649e0a
    Binary files /dev/null and b/bin/jp/gab_km/MoneyTest.class differ
    diff --git a/src/jp/gab_km/Dollar.java b/src/jp/gab_km/Dollar.java
    deleted file mode 100644
    index 5898fad..0000000
    --- a/src/jp/gab_km/Dollar.java
    +++ /dev/null
    @@ -1,9 +0,0 @@
    -package jp.gab_km;
    -
    -public class Dollar extends Money {
    -
    -	public Dollar(int amount, String currency) {
    -		super(amount, currency);
    -	}
    -
    -}
    diff --git a/src/jp/gab_km/DollarTest.java b/src/jp/gab_km/DollarTest.java
    deleted file mode 100644
    index c6e5011..0000000
    --- a/src/jp/gab_km/DollarTest.java
    +++ /dev/null
    @@ -1,28 +0,0 @@
    -package jp.gab_km;
    -
    -import static org.junit.Assert.*;
    -
    -import org.junit.Test;
    -
    -public class DollarTest {
    -
    -	@Test
    -	public void testMultiplication() {
    -		Money five = Money.dollar(5);
    -		assertEquals(Money.dollar(10), five.times(2));
    -		assertEquals(Money.dollar(15), five.times(3));
    -	}
    -
    -	@Test
    -	public void testEquality(){
    -		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
    -		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
    -		assertFalse(Money.franc(5).equals(Money.dollar(5)));
    -	}
    -
    -	@Test
    -	public void testCurrency(){
    -		assertEquals("USD", Money.dollar(1).currency());
    -	}
    -
    -}
    diff --git a/src/jp/gab_km/MoneyTest.java b/src/jp/gab_km/MoneyTest.java
    new file mode 100644
    index 0000000..b18a963
    --- /dev/null
    +++ b/src/jp/gab_km/MoneyTest.java
    @@ -0,0 +1,28 @@
    +package jp.gab_km;
    +
    +import static org.junit.Assert.*;
    +
    +import org.junit.Test;
    +
    +public class MoneyTest {
    +
    +	@Test
    +	public void testMultiplication() {
    +		Money five = Money.dollar(5);
    +		assertEquals(Money.dollar(10), five.times(2));
    +		assertEquals(Money.dollar(15), five.times(3));
    +	}
    +
    +	@Test
    +	public void testEquality(){
    +		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
    +		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
    +		assertFalse(Money.franc(5).equals(Money.dollar(5)));
    +	}
    +
    +	@Test
    +	public void testCurrency(){
    +		assertEquals("USD", Money.dollar(1).currency());
    +	}
    +
    +}

--- src/jp/gab_km/Dollar.java
+++
@@ -1,9 +0,0 @@
-package jp.gab_km;
-
-public class Dollar extends Money {
-	
-	public Dollar(int amount, String currency) {
-		super(amount, currency);
-	}
-
-}
--- src/jp/gab_km/MoneyTest.java
+++ src/jp/gab_km/MoneyTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class DollarTest {
+public class MoneyTest {
 
 	@Test
 	public void testMultiplication() {
