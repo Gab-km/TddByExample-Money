cd02278ed54459de4a2e06fc87186bd46d40ea28
Gab_km <the25thcromosome@gmail.com>
2012-02-18 01:44:54 +0900

    [from now] Sat Feb 18 01:44:54     2012
    diff --git a/bin/jp/gab_km/Bank.class b/bin/jp/gab_km/Bank.class
    index 5e9f648..70c0cf3 100644
    Binary files a/bin/jp/gab_km/Bank.class and b/bin/jp/gab_km/Bank.class differ
    diff --git a/bin/jp/gab_km/MoneyTest.class b/bin/jp/gab_km/MoneyTest.class
    index a7439b5..d799206 100644
    Binary files a/bin/jp/gab_km/MoneyTest.class and b/bin/jp/gab_km/MoneyTest.class differ
    diff --git a/src/jp/gab_km/Bank.java b/src/jp/gab_km/Bank.java
    index 0a113ad..58da769 100644
    --- a/src/jp/gab_km/Bank.java
    +++ b/src/jp/gab_km/Bank.java
    @@ -3,6 +3,7 @@ package jp.gab_km;
     public class Bank {
    
     	public Money reduce(Expression source, String to) {
    +		if (source instanceof Money) return (Money) source;
     		Sum sum = (Sum) source;
     		return sum.reduce(to);
     	}
    diff --git a/src/jp/gab_km/MoneyTest.java b/src/jp/gab_km/MoneyTest.java
    index 570c85a..6b56004 100644
    --- a/src/jp/gab_km/MoneyTest.java
    +++ b/src/jp/gab_km/MoneyTest.java
    @@ -57,5 +57,12 @@ public class MoneyTest {
     		Money result = bank.reduce(sum, "USD");
     		assertEquals(Money.dollar(7), result);
     	}
    +
    +	@Test
    +	public void testReduceMoney(){
    +		Bank bank = new Bank();
    +		Money result = bank.reduce(Money.dollar(1), "USD");
    +		assertEquals(Money.dollar(1), result);
    +	}
    
     }

--- src/jp/gab_km/Bank.java
+++ src/jp/gab_km/Bank.java
@@ -3,6 +3,7 @@ package jp.gab_km;
 public class Bank {
 
 	public Money reduce(Expression source, String to) {
+		if (source instanceof Money) return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
