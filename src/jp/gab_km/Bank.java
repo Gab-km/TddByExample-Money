08f59923784c1382d82227755b4cefa06e200798
Gab_km <the25thcromosome@gmail.com>
2012-02-19 00:41:37 +0900

    [from now] Sun Feb 19 00:41:37     2012
    diff --git a/bin/jp/gab_km/Bank.class b/bin/jp/gab_km/Bank.class
    index f5b4132..4b913c2 100644
    Binary files a/bin/jp/gab_km/Bank.class and b/bin/jp/gab_km/Bank.class differ
    diff --git a/bin/jp/gab_km/Pair.class b/bin/jp/gab_km/Pair.class
    new file mode 100644
    index 0000000..93ebe0d
    Binary files /dev/null and b/bin/jp/gab_km/Pair.class differ
    diff --git a/src/jp/gab_km/Bank.java b/src/jp/gab_km/Bank.java
    index d7d524a..0bd2a6f 100644
    --- a/src/jp/gab_km/Bank.java
    +++ b/src/jp/gab_km/Bank.java
    @@ -1,6 +1,10 @@
     package jp.gab_km;
    
    +import java.util.HashMap;
    +
     public class Bank {
    +
    +	private HashMap<Pair, Integer> rates = new HashMap<>();
    
     	public Money reduce(Expression source, String to) {
     		return source.reduce(this, to);
    @@ -10,9 +14,8 @@ public class Bank {
     		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
     	}
    
    -	public void addRate(String string, String string2, int i) {
    -		// TODO Auto-generated method stub
    -
    +	public void addRate(String from, String to, int rate) {
    +		rates.put(new Pair(from, to), new Integer(rate));
     	}
    
     }
    diff --git a/src/jp/gab_km/Pair.java b/src/jp/gab_km/Pair.java
    new file mode 100644
    index 0000000..4695c71
    --- /dev/null
    +++ b/src/jp/gab_km/Pair.java
    @@ -0,0 +1,20 @@
    +package jp.gab_km;
    +
    +public class Pair {
    +	private String from;
    +	private String to;
    +
    +	Pair(String from, String to) {
    +		this.from = from;
    +		this.to = to;
    +	}
    +
    +	public boolean equals(Object object) {
    +		Pair pair = (Pair) object;
    +		return from.equals(pair.from) && to.equals(pair.to);
    +	}
    +
    +	public int hashCode() {
    +		return 0;
    +	}
    +}

--- src/jp/gab_km/Bank.java
+++ src/jp/gab_km/Bank.java
@@ -1,6 +1,10 @@
 package jp.gab_km;
 
+import java.util.HashMap;
+
 public class Bank {
+	
+	private HashMap<Pair, Integer> rates = new HashMap<>();
 
 	public Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
@@ -10,9 +14,8 @@ public class Bank {
 		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
 	}
 
-	public void addRate(String string, String string2, int i) {
-		// TODO Auto-generated method stub
-
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), new Integer(rate));
 	}
 
 }
