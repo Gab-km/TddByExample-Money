91d3716746f2b8d02e6a0f5f066da7df463c6649
Gab_km <the25thcromosome@gmail.com>
2012-02-19 00:21:23 +0900

    [from now] Sun Feb 19 00:21:24     2012
    diff --git a/bin/jp/gab_km/MoneyTest.class b/bin/jp/gab_km/MoneyTest.class
    index 5ae49f5..f4e1236 100644
    Binary files a/bin/jp/gab_km/MoneyTest.class and b/bin/jp/gab_km/MoneyTest.class differ
    diff --git a/src/jp/gab_km/MoneyTest.java b/src/jp/gab_km/MoneyTest.java
    index e3a936c..ba1aa6c 100644
    --- a/src/jp/gab_km/MoneyTest.java
    +++ b/src/jp/gab_km/MoneyTest.java
    @@ -14,24 +14,24 @@ public class MoneyTest {
     	}
    
     	@Test
    -	public void testFrancMultiplication(){
    +	public void testFrancMultiplication() {
     		Money five = Money.franc(5);
     		assertEquals(Money.franc(10), five.times(2));
     		assertEquals(Money.franc(15), five.times(3));
     	}
    -
    +
     	@Test
    -	public void testEquality(){
    +	public void testEquality() {
     		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
     		assertFalse(Money.franc(5).equals(Money.dollar(5)));
     	}
    -
    +
     	@Test
    -	public void testCurrency(){
    +	public void testCurrency() {
     		assertEquals("USD", Money.dollar(1).currency());
     	}
    -
    +
     	@Test
     	public void testSimpleAddition() {
     		Money five = Money.dollar(5);
    @@ -40,31 +40,31 @@ public class MoneyTest {
     		Money reduced = bank.reduce(sum, "USD");
     		assertEquals(Money.dollar(10), reduced);
     	}
    -
    +
     	@Test
    -	public void testPlusReturnsSUm(){
    +	public void testPlusReturnsSUm() {
     		Money five = Money.dollar(5);
     		Expression result = five.plus(five);
     		Sum sum = (Sum) result;
     		assertEquals(five, sum.augend);
     		assertEquals(five, sum.addend);
     	}
    -
    +
     	@Test
    -	public void testReduceSum(){
    +	public void testReduceSum() {
     		Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
     		Bank bank = new Bank();
     		Money result = bank.reduce(sum, "USD");
     		assertEquals(Money.dollar(7), result);
     	}
    -
    +
     	@Test
    -	public void testReduceMoney(){
    +	public void testReduceMoney() {
     		Bank bank = new Bank();
     		Money result = bank.reduce(Money.dollar(1), "USD");
     		assertEquals(Money.dollar(1), result);
     	}
    -
    +
     	@Test
     	public void testReduceMoneyDifferentCurrency() {
     		Bank bank = new Bank();
    @@ -73,4 +73,9 @@ public class MoneyTest {
     		assertEquals(Money.dollar(1), result);
     	}
    
    +	@Test
    +	public void testArrayEquals() {
    +		assertEquals(new Object[] { "abc" }, new Object[] { "abc" });
    +	}
    +
     }

--- src/jp/gab_km/MoneyTest.java
+++ src/jp/gab_km/MoneyTest.java
@@ -14,24 +14,24 @@ public class MoneyTest {
 	}
 
 	@Test
-	public void testFrancMultiplication(){
+	public void testFrancMultiplication() {
 		Money five = Money.franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 	}
-	
+
 	@Test
-	public void testEquality(){
+	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
-	
+
 	@Test
-	public void testCurrency(){
+	public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 	}
-	
+
 	@Test
 	public void testSimpleAddition() {
 		Money five = Money.dollar(5);
@@ -40,37 +40,42 @@ public class MoneyTest {
 		Money reduced = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(10), reduced);
 	}
-	
+
 	@Test
-	public void testPlusReturnsSUm(){
+	public void testPlusReturnsSUm() {
 		Money five = Money.dollar(5);
 		Expression result = five.plus(five);
 		Sum sum = (Sum) result;
 		assertEquals(five, sum.augend);
 		assertEquals(five, sum.addend);
 	}
-	
+
 	@Test
-	public void testReduceSum(){
+	public void testReduceSum() {
 		Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
 		Bank bank = new Bank();
 		Money result = bank.reduce(sum, "USD");
 		assertEquals(Money.dollar(7), result);
 	}
-	
+
 	@Test
-	public void testReduceMoney(){
+	public void testReduceMoney() {
 		Bank bank = new Bank();
 		Money result = bank.reduce(Money.dollar(1), "USD");
 		assertEquals(Money.dollar(1), result);
 	}
-	
+
 	@Test
 	public void testReduceMoneyDifferentCurrency() {
 		Bank bank = new Bank();
 		bank.addRate("CHF", "USD", 2);
 		Money result = bank.reduce(Money.franc(2), "USD");
 		assertEquals(Money.dollar(1), result);
+	}
+
+	@Test
+	public void testArrayEquals() {
+		assertEquals(new Object[] { "abc" }, new Object[] { "abc" });
 	}
 
 }
